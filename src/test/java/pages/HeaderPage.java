f9d81b864c2503fd97171ffaa03534d94caf23ae
neversometimes <128316603+neversometimes@users.noreply.github.com>
2024-08-22 23:44:19 -0700

    init commit.

--- src/test/java/pages/HeaderPage.java
+++ src/test/java/pages/HeaderPage.java
@@ -1,4 +1,79 @@
 package pages;
 
-public class HeaderPage {
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HeaderPage extends BasePage {
+
+    public HeaderPage (WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    // Page Factory Declarations
+    @FindBy(css=".btn.btn-custom[routerlink='/dashboard/']")
+    WebElement homeBtn;
+
+    @FindBy(css=".btn.btn-custom[routerlink='/dashboard/myorders']")
+    WebElement ordersBtn;
+
+    @FindBy(css=".btn.btn-custom[routerlink='/dashboard/cart']")
+    WebElement cartBtn;
+
+    @FindBy(css="li:nth-child(5) button:nth-child(1)")
+    WebElement signOutBtn;
+
+    @FindBy(css=".mt-4.ng-star-inserted")
+    WebElement ordersPageTxt;
+
+    @FindBy(css="div[class='ng-star-inserted'] h1")
+    WebElement emptyCartPageTxt;
+
+    @FindBy(css="#toast-container")
+    WebElement cartToastTxt;
+
+    @FindBy(css="ng-animating")
+    WebElement toastVanishes;
+
+    @FindBy(css=".m-2.blink_me")
+    WebElement homePageTxt;
+
+    // Defined Page Actions
+    public void clickHomeBtn() {
+        homeBtn.click();
+    }
+    public String getHomePageTxt() {
+        return homePageTxt.getText();
+    }
+    public void clickOrdersBtn() {
+        ordersBtn.click();
+        waitForWebElementToDisappear(homePageTxt);
+    }
+    public String getOrdersPageTxt() {
+        return ordersPageTxt.getText();
+    }
+    public void clickCartBtn() {
+        cartBtn.click();
+    }
+    public String getCartToastTxt() {
+        waitForWebElementToAppear(cartToastTxt);
+        String s = cartToastTxt.getText();
+        waitForWebElementToDisappear(toastVanishes);
+        return s;
+    }
+
+    public String getCartPageTxt() {
+        return emptyCartPageTxt.getText();
+    }
+    public void clickSignOutBtn() {
+        signOutBtn.click();
+    }
+
+
+
+
+
 }
---
+++ src/test/java/tests/HeaderTest.java
@@ -0,0 +1,49 @@
+package tests;
+
+import base.BaseTests;
+import pages.HeaderPage;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class HeaderTest extends BaseTests {
+
+    // This test class navigates E2E exclusive through the
+    // header links: Home/Orders/Cart/SignOut
+
+    @Test
+    public void navigateHeaderLinks() {
+        HeaderPage headerPage = new HeaderPage(driver);
+        doLogIn();
+
+        // click on Orders
+        headerPage.clickOrdersBtn();
+        // verify page navigates to Orders page
+        System.out.println(headerPage.getOrdersPageTxt());
+        assertEquals(headerPage.getOrdersPageTxt(),
+                "You have No Orders to show at this time.\nPlease Visit Back Us");
+
+        // click on Home
+        headerPage.clickHomeBtn();
+        // verify back at home app page
+        assertEquals(headerPage.getHomePageTxt(),
+                "User can only see maximum 9 products on a page");
+
+        // click on Cart
+        headerPage.clickCartBtn();
+        // verify navigates to Cart page
+        assertEquals(headerPage.getCartToastTxt(), "No Product in Your Cart");
+        assertEquals(headerPage.getCartPageTxt(), "No Products in Your Cart !");
+
+        // click on Home
+        headerPage.clickHomeBtn();
+        // verify back at home app page
+        assertEquals(headerPage.getHomePageTxt(),
+                "User can only see maximum 9 products on a page");
+
+        // click sign out btn
+        headerPage.clickSignOutBtn();
+        // verify at login page
+        assertEquals(getPageURL(),appLoginPageURL);
+    }
+
+}

fcfd8172aa2a607835fa277325bc27012348a13d
neversometimes <128316603+neversometimes@users.noreply.github.com>
2024-08-24 15:25:04 -0700

    Initial commit.

--- src/test/java/pages/PaymentPage.java
+++ src/test/java/pages/PaymentPage.java
@@ -1,4 +1,173 @@
 package pages;
 
-public class PaymentPage {
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PaymentPage extends BasePage {
+
+    public PaymentPage (WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    // Page Factory Declarations
+    @FindBy(css=".item__title")
+    WebElement prodName;
+
+    @FindBy(css="label[type='text']")
+    WebElement shippingInfoName;
+
+    @FindBy(css="input[placeholder$='Select Country']")
+    WebElement countryNameInput;
+
+    @FindBy(css="span[class='ng-star-inserted']")
+    WebElement countryNameDropdown;
+
+    @FindBy(css=".btnn.action__submit.ng-star-inserted")
+    WebElement placeOrderBtn;
+
+    @FindBy(css=".hero-primary")
+    WebElement orderConfirmationBannerTxt;
+
+    @FindBy(css="td[class='line-item product-info-column m-3'] div[class='title']")
+    WebElement orderConfirmProductTxt;
+
+    @FindBy(css="label[routerlink='/dashboard/myorders']")
+    WebElement ordersHistoryPageLink;
+
+    @FindBy(css="tbody tr:nth-child(4) button")
+    WebElement downloadOrderCSVBtn;
+
+    @FindBy(css="tbody tr:nth-child(5) button")
+    WebElement downloadOrderXLBtn;
+
+    @FindBy(css="#toast-container")
+    WebElement cartToastTxt;
+
+    @FindBy(css=".ng-animating")
+    WebElement toastVanishes;
+
+    @FindBy(css="tr td:nth-of-type(2)")
+    WebElement orderNameTxt;
+
+    @FindBy(css="tr td:nth-of-type(3)")
+    WebElement orderPriceTxt;
+
+    @FindBy(css="tr td:nth-of-type(4)")
+    WebElement orderDateTxt;
+
+    @FindBy(css="button[class='btn btn-primary']")
+    WebElement orderViewBtn;
+
+    @FindBy(css="tr td:nth-of-type(6) button")
+    WebElement orderDeleteBtn;
+
+    @FindBy(css=".btn.btn-primary.col-md-2.offset-md-4")
+    WebElement orderGoBackToShopBtn;
+
+    @FindBy(css="div[class='row'] button:last-of-type")
+    WebElement orderGoBackToCartBtn;
+
+    @FindBy(xpath="//div[@class='email-wrapper ng-star-inserted']//div[1]//div[1]//p[1]")
+    WebElement billingAddrNameTxt;
+
+    @FindBy(xpath="//div[@class='email-wrapper ng-star-inserted']//div[1]//div[1]//p[2]")
+    WebElement billingCountryTxt;
+
+    @FindBy(xpath="//div[@class='email-container']//div[2]//div[1]//p[1]")
+    WebElement deliveryAddrNameTxt;
+
+    @FindBy(xpath="//div[@class='email-container']//div[2]//div[1]//p[2]")
+    WebElement deliveryCountryTxt;
+
+    @FindBy(css=".title")
+    WebElement orderProdName;
+
+    @FindBy(css=".btn.-teal")
+    WebElement viewOrdersBtn;
+
+    // ** Defined Page Actions **
+
+    public String getProdNameTxt() {
+        return prodName.getText();
+    }
+    public String getShippingInfoNameTxt() {
+        return shippingInfoName.getText();
+    }
+    public void setCountryNameInput() {
+        countryNameInput.sendKeys("Canada");
+        countryNameDropdown.click();
+    }
+    public void clickPlaceOrderBtn() {
+        placeOrderBtn.click();
+    }
+    public String getOrderConfirmationBannerTxt() {
+        return orderConfirmationBannerTxt.getText();
+    }
+    public String getOrderConfirmProductTxt(){
+        return orderConfirmProductTxt.getText();
+    }
+    public void clickOrdersHistoryPageLink() {
+        ordersHistoryPageLink.click();
+    }
+    public WebElement clickDownloadOrderCSVBtn() {
+        downloadOrderCSVBtn.click();
+        return downloadOrderCSVBtn;
+    }
+    public void clickDownloadOrderXLBtn() {
+        downloadOrderXLBtn.click();
+    }
+    public String getPaymentToastTxt() {
+        waitForWebElementToAppear(cartToastTxt);
+        String s = cartToastTxt.getText();
+        waitForWebElementToDisappear(toastVanishes);
+        return s;
+    }
+    public String getOrderNameTxt() {
+        return orderNameTxt.getText();
+    }
+    public String getOrderPriceTxt() {
+        return orderPriceTxt.getText();
+    }
+    public String getOrderDateTxt() {
+        return orderDateTxt.getText();
+    }
+    public void clickOrderViewBtn() {
+        orderViewBtn.click();
+    }
+    public void clickOrderDeleteBtn() {
+        orderDeleteBtn.click();
+    }
+    public void clickBackToShopBtn() {
+        orderGoBackToShopBtn.click();
+        waitForWebElementToDisappear(orderGoBackToShopBtn);
+    }
+    public void clickBackToCartBtn() {
+        orderGoBackToCartBtn.click();
+        waitForWebElementToDisappear(orderGoBackToCartBtn);
+    }
+    public String getBillingAddrNameTxt() {
+        return billingAddrNameTxt.getText();
+    }
+    public String getBillingCountryTxt(){
+        return billingCountryTxt.getText();
+    }
+    public String getDeliveryAddrNameTxt() {
+        return deliveryAddrNameTxt.getText();
+    }
+    public String getDeliveryCountryTxt() {
+        return deliveryCountryTxt.getText();
+    }
+    public String getOrderProdNameTxt() {
+        return orderProdName.getText();
+    }
+    public void clickViewOrdersBtn() {
+        viewOrdersBtn.click();
+    }
+
+
+
 }
---
+++ src/test/java/tests/PaymentTest.java
@@ -0,0 +1,195 @@
+package tests;
+
+import base.BaseTests;
+import pages.PaymentPage;
+import pages.CartPage;
+import pages.HeaderPage;
+
+import org.testng.annotations.Test;
+import pages.ProductViewPage;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.testng.Assert.*;
+
+
+public class PaymentTest extends BaseTests {
+
+    //  ** Payment Page Order Pre-requisite **
+    public void placeOrderPrep() throws Exception {
+        CartPage crtPage = new CartPage(driver);
+        ProductViewPage pvPage = new ProductViewPage(driver);
+
+        doLogIn();
+
+        // select checkbox for shoes
+        pvPage.clickSubCatShoesChkbx();     //isolate to just one product
+        // click view product button
+        pvPage.clickViewShoeBtn();
+
+        // from Product page, click Add To Product button
+        crtPage.clickProdPageAddToCartBtn();
+
+        // wait and verify toast message "Product Added To Cart"
+        assertEquals(crtPage.getCartToastTxt(), "Product Added To Cart");
+
+        // verify cart button shows 1 item
+        assertEquals(crtPage.getCartCountTxt(), "1");
+
+        // go to Cart page
+        crtPage.clickCartBtn();
+
+        // verify item added to Cart is Shoes
+        assertEquals(crtPage.getCartItemTxt(), "ADIDAS ORIGINAL");
+
+        // click Buy Now
+        crtPage.clickBuyNowBtn();
+
+    }
+
+    //  ** Payment Page Verifications **
+    @Test
+    public void paymentPlaceOrder() throws Exception {
+        placeOrderPrep();
+
+        PaymentPage paymentPage = new PaymentPage(driver);
+
+        // verify cart product is the one being ordered
+        assertEquals(paymentPage.getProdNameTxt(), "ADIDAS ORIGINAL");
+
+        // verify shipping info name = "username@ms.com" (logged-in user email)
+        assertEquals(paymentPage.getShippingInfoNameTxt(), "username@ms.com");
+
+        // enter "Canada" in "Select Country" and click "Canada" dropdown
+        paymentPage.setCountryNameInput();
+
+        // click "PLACE ORDER" btn
+        paymentPage.clickPlaceOrderBtn();
+        assertEquals(paymentPage.getPaymentToastTxt(), "Order Placed Successfully");
+
+        // verify order confirmation page
+        assertEquals(paymentPage.getOrderConfirmationBannerTxt(), "THANKYOU FOR THE ORDER.");
+        // verify order info reflects correct product ordered
+        assertEquals(paymentPage.getOrderConfirmProductTxt(), "ADIDAS ORIGINAL");
+
+        // click Orders History Page link
+        paymentPage.clickOrdersHistoryPageLink();
+        // verify myOrders Page
+        assertEquals(getPageURL(), myOrdersPageURL);
+    }
+
+    @Test void orderClearsCart() throws Exception {
+        placeOrderPrep();
+        CartPage crtPage = new CartPage(driver);
+        PaymentPage paymentPage = new PaymentPage(driver);
+        HeaderPage hdrPage = new HeaderPage(driver);
+
+        // verify cart product is the one being ordered
+        assertEquals(paymentPage.getProdNameTxt(), "ADIDAS ORIGINAL");
+
+        // verify shipping info name = "username@ms.com" (logged-in user email)
+        assertEquals(paymentPage.getShippingInfoNameTxt(), "username@ms.com");
+
+        // enter "Canada" in "Select Country" and click "Canada" dropdown
+        paymentPage.setCountryNameInput();
+
+        // click "PLACE ORDER" btn
+        paymentPage.clickPlaceOrderBtn();
+        assertEquals(paymentPage.getPaymentToastTxt(), "Order Placed Successfully");
+
+        // click Cart btn
+        crtPage.clickCartBtn();
+
+        // verify No Product toast
+        assertEquals(crtPage.getCartToastTxt(), "No Product in Your Cart");
+        // verify No Products page text
+        assertEquals(crtPage.getNoProductTxt(), "No Products in Your Cart !");
+
+        // test clean up : delete the item in the OrderList
+        // click Orders Header btn
+        hdrPage.clickOrdersBtn();
+        // click delete btn in Orders page
+        paymentPage.clickOrderDeleteBtn();
+
+    }
+    @Test
+    public void ordersPageDetails() throws Exception {
+        paymentPlaceOrder();
+        PaymentPage paymentPage = new PaymentPage(driver);
+
+        Date date = new Date();
+
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        sdf.applyPattern("E MMM dd");
+
+        // TODO : current date needs to be current UTC date, not local date
+
+        // verify order details
+        assertEquals(paymentPage.getOrderNameTxt(), "ADIDAS ORIGINAL");
+        assertEquals(paymentPage.getOrderPriceTxt(), "$ 31500");
+        assertEquals(paymentPage.getOrderDateTxt(), sdf.format(date));
+
+    }
+    @Test
+    public void ordersPageDelete() throws Exception {
+        paymentPlaceOrder();
+        PaymentPage paymentPage = new PaymentPage(driver);
+        HeaderPage hdrPage = new HeaderPage(driver);
+        CartPage crtPage = new CartPage(driver);
+
+        // click "Go Back to Shop"
+        paymentPage.clickBackToShopBtn();
+        // verify at home shop page
+        assertEquals(getPageURL(), homePageURL);
+        // click ORDERS in header to return to Orders page
+        hdrPage.clickOrdersBtn();
+
+        //click delete btn
+        paymentPage.clickOrderDeleteBtn();
+        // verify toast txt "Orders Deleted Successfully"
+        assertEquals(crtPage.getCartToastTxt(), "Orders Deleted Successfully");
+        // verify "You have No Orders to show at this time.\n Please Visit Back Us" text
+        assertEquals(hdrPage.getOrdersPageTxt(), "You have No Orders to show at this time.\nPlease Visit Back Us");
+
+        // click "Go Back to Cart"
+        paymentPage.clickBackToCartBtn();
+
+        // verify page text "No Products in Your Cart!"
+        assertEquals(crtPage.getNoProductTxt(), "No Products in Your Cart !");
+
+        // click "Go Back to Shop" (from Cart page)
+        crtPage.clickContinueShopping();
+        // verify at home shop page
+        assertEquals(getPageURL(), homePageURL);
+
+    }
+    @Test
+    public void ordersPageSummary() throws Exception {
+        paymentPlaceOrder();
+        PaymentPage paymentPage = new PaymentPage(driver);
+
+        // click "View" button on Orders Page
+        paymentPage.clickOrderViewBtn();
+
+        // ** Orders Summary Page **
+
+        // verify billing and delivery address and country
+        assertEquals(paymentPage.getBillingAddrNameTxt(), "username@ms.com");
+        assertEquals(paymentPage.getBillingCountryTxt(), "Country - Canada");
+        assertEquals(paymentPage.getDeliveryAddrNameTxt(), "username@ms.com");
+        assertEquals(paymentPage.getDeliveryCountryTxt(), "Country - Canada");
+
+        // verify product ordered name
+        assertEquals(paymentPage.getOrderProdNameTxt(), "ADIDAS ORIGINAL");
+
+        // click "View Orders" button
+        paymentPage.clickViewOrdersBtn();
+        // verify nav to Orders Page
+        assertEquals(getPageURL(), myOrdersPageURL);
+
+        // clean up:  delete the order
+        paymentPage.clickOrderDeleteBtn();
+
+    }
+}

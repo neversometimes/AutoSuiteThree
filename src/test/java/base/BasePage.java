f75917af8555db0d2bbf2d5e272d32a28dcddbb4
neversometimes <128316603+neversometimes@users.noreply.github.com>
2024-08-25 12:06:32 -0700

    added ScrollToElement()

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -1,6 +1,6 @@
 package base;
 
-
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -24,6 +24,12 @@ public class BasePage {
     public void waitForWebElementToDisappear(WebElement element) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    public void scrollToElement(WebElement webElement) throws Exception {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String script = "arguments[0].scrollIntoView();";
+        js.executeScript (script, webElement);
     }
 
 }

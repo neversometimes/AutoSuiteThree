beadbf7aad8e18e7afc40de5f548a27826eb5626
neversometimes <128316603+neversometimes@users.noreply.github.com>
2024-08-30 14:55:38 -0700

    added groups='bvt' to @Test method added groups='functional' to @Test class

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -6,6 +6,7 @@ import pages.LoginPage;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
+@Test(groups = {"functional"})
 public class LoginTest extends BaseTests {
     // This class includes a basic set of tests for logging into the web app
     // TODO - E2E scenario:
@@ -13,7 +14,7 @@ public class LoginTest extends BaseTests {
     // TODO - E2E scenario:
     // after new password set, verify login
 
-    @Test
+    @Test(groups={"bvt"})
     public void loginNoCreds() {
         LoginPage loginPage = new LoginPage(driver);
 
@@ -24,7 +25,7 @@ public class LoginTest extends BaseTests {
         assertEquals(loginPage.getEmailErrorTxt(), "*Email is required");
         assertEquals(loginPage.getPwdErrorTxt(), "*Password is required");
     }
-    @Test
+    @Test(groups={"bvt"})
     public void loginBadCreds() throws Exception {
         LoginPage loginPage = new LoginPage(driver);
 
@@ -35,10 +36,10 @@ public class LoginTest extends BaseTests {
         // click loginBtn and wait to verify toast error message
         loginPage.clickLoginBtn();
         assertEquals(loginPage.getToastTxt(), "Incorrect email or password.");
-
     }
-    @Test(retryAnalyzer = Retry.class)
+    @Test(groups={"bvt"}, retryAnalyzer = Retry.class)
     public void basicLogIn() throws Exception {
+
         LoginPage loginPage = new LoginPage(driver);
         // pre-requisite: assumes valid registered user credentials
         loginPage.enterEmail("username@ms.com");
